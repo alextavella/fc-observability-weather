38acfeb55d6baa5bd88763f24625ec86aca66eb7
alextavella <alex.tavella@azos.com.br>
2025-05-06 21:50:11 -0300

    refactor: use fiber

--- cmd/a/main.go
+++ cmd/a/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	otel "github.com/alextavella/go-opentelemetry/pkg/otel"
 	"github.com/gofiber/fiber/v3"
@@ -25,23 +27,29 @@ func main() {
 		log.Fatalf("Erro ao configurar OpenTelemetry: %v", err)
 	}
 
-	// Shutdown gracefully on interrupt signal
-	shuCh := make(chan os.Signal, 1)
-	go func() {
-		<-shuCh
-		log.Println("Recebido sinal de desligamento, encerrando...")
-		if err := otelShutdown(); err != nil {
-			log.Fatalf("Erro ao desligar OpenTelemetry: %v", err)
-		}
-		log.Println("OpenTelemetry desligado com sucesso")
-		os.Exit(0)
-	}()
-
 	// HTTP Server
 	app := fiber.New()
 	app.Get("/health", func(c fiber.Ctx) error {
 		return c.SendString("OK")
 	})
+
+	// Shutdown
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigCh
+		log.Println("Received shutdown signal, shutting down...")
+		if err := otelShutdown(); err != nil {
+			log.Fatalf("Error shutting down OpenTelemetry: %v", err)
+		}
+		log.Println("OpenTelemetry shut down successfully")
+		if err := app.Shutdown(); err != nil {
+			log.Fatalf("Error shutting down: %v", err)
+		}
+		log.Println("Server shut down gracefully")
+	}()
+
+	// Start server
 	log.Println("Servidor ouvindo em :8080")
 	log.Fatal(app.Listen(":8080"))
 }
